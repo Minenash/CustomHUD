1fd206ae6b523c8cce06c29ec656856b9cc6720f
minenash <minenash@protonmail.com>
2022-05-22 22:18:13 -0400

    Fix #38

--- src/main/java/com/minenash/customhud/CustomHudRenderer.java
+++ src/main/java/com/minenash/customhud/CustomHudRenderer.java
@@ -79,7 +79,8 @@ public class CustomHudRenderer {
                             continue;
                         }
 
-                        int baseX = (left ? 5 : (int)(client.getWindow().getScaledHeight()*(1/profile.scale)) - 3 - totalWidth) + profile.offsets[i][0];
+
+                        int baseX = (left ? 5 : (int)(client.getWindow().getScaledWidth()*(1/profile.scale)) - 3 - totalWidth) + profile.offsets[i][0];
                         DrawableHelper.fill(matrix, baseX - 2, y, baseX + lineLength(profile,i,totalWidth) + 1, y + 9 + profile.lineSpacing, profile.bgColor);
 
                         int xOffset = 0;
