59e5de39d4598a18951ede228f14bb2a0ea8983c
minenash <minenash@protonmail.com>
2023-11-17 17:03:03 -0500

    More work on new config screen

--- src/main/java/com/minenash/customhud/gui/NewConfigScreen.java
+++ src/main/java/com/minenash/customhud/gui/NewConfigScreen.java
@@ -4,27 +4,18 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import org.lwjgl.glfw.GLFW;
 
 public class NewConfigScreen extends Screen {
 
-    private static final int OPTION_START = 32+13, OPTION_BUFFER = 30;
     private final Screen parent;
     private final TextRenderer font;
 
-    private final KeyBinding binding = new KeyBinding("test", GLFW.GLFW_KEY_UNKNOWN, "Test Cat");
-
-    private ButtonWidget keyButton;
-    private boolean profileActive = false;
-    private boolean inCycle = false;
-    private boolean keybindSelected = false;
+    private ProfileLinesWidget profiles;
+    public KeyBinding selectedKeybind;
+    public ProfileLineEntry active;
 
     public NewConfigScreen(Screen parent) {
         super(Text.translatable("sml.config.screen.title"));
@@ -34,35 +25,23 @@ public class NewConfigScreen extends Screen {
 
     @Override
     protected void init() {
-        example("○", 2+50, OPTION_START, 16, (button) -> {
-            profileActive = !profileActive; button.setMessage(Text.literal(profileActive ? "☑" : "☐"));});
-        example("Edit", width-34-50, OPTION_START, 32, (b) -> {});
-        example("☐", width-34-82-18-50, OPTION_START, 16, (button) -> {
-            inCycle = !inCycle; button.setMessage(Text.literal(inCycle ? "☑" : "☐"));});
-
-
-        keyButton = example(I18n.translate(binding.getBoundKeyTranslationKey()), width-34-82-50, OPTION_START, 80, (b) -> {
-            keybindSelected = true;
-            update();
-        });
+        profiles = new ProfileLinesWidget(this,30, height-20);
+        addSelectableChild(profiles);
     }
 
-    private void update() {
-        keyButton.setMessage(this.binding.getBoundKeyLocalizedText());
-        if (keybindSelected)
-//            keyButton.setMessage(keyButton.getMessage().copy().formatted(Formatting.YELLOW, Formatting.UNDERLINE));
-            keyButton.setMessage(Text.literal("> ")
-                    .append(keyButton.getMessage().copy().formatted(Formatting.WHITE, Formatting.UNDERLINE))
-                    .append(" <").formatted(Formatting.YELLOW));
-
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        context.drawCenteredTextWithShadow(font, Text.translatable("config.custom_hud.title"), this.width / 2, 13, 0xFFFFFF);
+        profiles.render(context, mouseX, mouseY, delta);
     }
 
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        if (keybindSelected) {
-            binding.setBoundKey(InputUtil.Type.MOUSE.createFromCode(button));
-            keybindSelected = false;
-            update();
+        if (selectedKeybind != null) {
+            selectedKeybind.setBoundKey(InputUtil.Type.MOUSE.createFromCode(button));
+            selectedKeybind = null;
+            profiles.update();
             return true;
         }
         return super.mouseClicked(mouseX, mouseY, button);
@@ -70,32 +49,18 @@ public class NewConfigScreen extends Screen {
 
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (keybindSelected) {
-            if (keyCode == GLFW.GLFW_KEY_ESCAPE)
-                binding.setBoundKey(InputUtil.UNKNOWN_KEY);
-            else
-                binding.setBoundKey(InputUtil.fromKeyCode(keyCode, scanCode));
-            keybindSelected = false;
-            update();
+        if (selectedKeybind != null) {
+            selectedKeybind.setBoundKey(keyCode == 256 ? InputUtil.UNKNOWN_KEY : InputUtil.fromKeyCode(keyCode, scanCode));
+            selectedKeybind = null;
+            profiles.update();
             return true;
         }
         return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
-    private ButtonWidget example(String text, int x, int y, int width, ButtonWidget.PressAction action) {
-        return addDrawableChild(ButtonWidget.builder(Text.literal(text), action)
-                .dimensions(x, y, width, 16)
-                .build());
-    }
-
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        context.drawCenteredTextWithShadow(font, Text.translatable("config.custom_hud.title"), this.width / 2, 13, 0xFFFFFF);
-
-//        context.drawTextWithShadow(font, Text.literal("○"), 2, OPTION_START + 5, 0xFFFFFF);
-        context.drawTextWithShadow(font, Text.literal("Profile 1"), 20+50, OPTION_START + 5, 0xFFFFFF);
-//        context.drawTextWithShadow(font, Text.literal("☐"), width-42-42-22, OPTION_START + 5, 0xFFFFFF);
-
-        super.render(context, mouseX, mouseY, delta);
+    public void close() {
+        client.setScreen(parent);
+        super.close();
     }
 }
--- src/main/java/com/minenash/customhud/gui/ProfileLineEntry.java
+++ src/main/java/com/minenash/customhud/gui/ProfileLineEntry.java
@@ -3,11 +3,8 @@ package com.minenash.customhud.gui;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.Selectable;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.option.ControlsListWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.ElementListWidget;
-import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
@@ -17,35 +14,33 @@ import java.util.List;
 
 import static com.minenash.customhud.CustomHud.CLIENT;
 
-public class ProfileLineWidget extends ElementListWidget.Entry<ProfileLineWidget> {
+public class ProfileLineEntry extends ElementListWidget.Entry<ProfileLineEntry> {
 
     private final KeyBinding binding;
-    private final String name;
+    private final Text name;
 
     private final ButtonWidget selected, cycled, keybind, edit;
 
-    private int screenWidth = 0;
-    private boolean profileActive = false;
+    private final ProfileLinesWidget widget;
     private boolean inCycle = false;
-    private boolean keybindSelected = false;
 
-    public ProfileLineWidget(String name, KeyBinding keyBinding, ElementListWidget screen) {
+    public ProfileLineEntry(String name, KeyBinding keyBinding, ProfileLinesWidget widget) {
         this.binding = keyBinding;
-        this.name = name;
-        this.screenWidth = screen.getRowWidth();
+        this.name = Text.literal(name);
+        this.widget = widget;
 
-        this.selected = button("☐", 16, (b) -> {profileActive = !profileActive; b.setMessage(Text.literal(profileActive ? "☑" : "☐"));});
-        this.edit = button("edit", 32, (b) -> {});
+        this.selected = button("☐", 16, (b) -> {widget.screen.active = this;});
+        this.edit = button("Edit", 32, (b) -> {});
         this.cycled = button("☐", 16, (b) -> {inCycle = !inCycle; b.setMessage(Text.literal(inCycle ? "☑" : "☐"));});
         this.keybind = button(I18n.translate(binding.getBoundKeyTranslationKey()), 80, (b) -> {
-            keybindSelected = true;
-            update();
+            widget.screen.selectedKeybind = binding;
+            widget.update();
         });
     }
 
-    private void update() {
+    void update() {
         keybind.setMessage(this.binding.getBoundKeyLocalizedText());
-        if (keybindSelected)
+        if (widget.screen.selectedKeybind == binding)
 //            keyButton.setMessage(keyButton.getMessage().copy().formatted(Formatting.YELLOW, Formatting.UNDERLINE));
             keybind.setMessage(Text.literal("> ")
                     .append(keybind.getMessage().copy().formatted(Formatting.WHITE, Formatting.UNDERLINE))
@@ -54,14 +49,14 @@ public class ProfileLineWidget extends ElementListWidget.Entry<ProfileLineWidget
     }
 
     @Override
-    public void render(DrawContext context, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
-        context.drawTextWithShadow(CLIENT.textRenderer, Text.literal("Profile 1"), x+20, y+5, 0xFFFFFF);
+    public void render(DrawContext context, int index, int y, int x, int eWidth, int eHeight, int mX, int mY, boolean hovered, float delta) {
+        context.drawTextWithShadow(CLIENT.textRenderer, name, x+20, y+4, 0xFFFFFF);
+        selected.setMessage(Text.literal(widget.screen.active == this ? "☑" : "☐"));
 
-        setPosAndRender(context, mouseX, mouseY, tickDelta, y, selected, 2);
-        setPosAndRender(context, mouseX, mouseY, tickDelta, y, cycled, -34);
-        setPosAndRender(context, mouseX, mouseY, tickDelta, y, keybind, -34-82);
-        setPosAndRender(context, mouseX, mouseY, tickDelta, y, edit, -34-82-18);
-
+        setPosAndRender(context, mX, mY, delta, x, y, eWidth, selected, 2);
+        setPosAndRender(context, mX, mY, delta, x, y, eWidth, edit, -34);
+        setPosAndRender(context, mX, mY, delta, x, y, eWidth, keybind, -34-82);
+        setPosAndRender(context, mX, mY, delta, x, y, eWidth, cycled, -34-82-18);
     }
 
     @Override public List<? extends Selectable> selectableChildren() { return List.of(selected, cycled, keybind, edit); }
@@ -71,8 +66,8 @@ public class ProfileLineWidget extends ElementListWidget.Entry<ProfileLineWidget
         return ButtonWidget.builder(Text.literal(text), action).dimensions(0, 0, width, 16).build();
     }
 
-    private void setPosAndRender(DrawContext context, int mouseX, int mouseY, float delta, int y, ButtonWidget widget, int x) {
-        widget.setX(x < 0 ? screenWidth - x - 50 : x + 50);
+    private void setPosAndRender(DrawContext context, int mouseX, int mouseY, float delta, int x, int y, int width, ButtonWidget widget, int xOffset) {
+        widget.setX(x + (xOffset < 0 ? width + xOffset : xOffset));
         widget.setY(y);
         widget.render(context, mouseX, mouseY, delta);
     }
